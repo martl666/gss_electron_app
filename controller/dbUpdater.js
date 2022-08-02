cc8b53b7959ebf83787dcf429970a0d1dd4dd125
Martin Haydn <martinhaydn@MacBook-Air-von-Martin.local>
2022-08-02 07:51:23 +0200

    check if db_storage dir is created and have right permissions

--- controller/dbUpdater.js
+++ controller/dbUpdater.js
@@ -6,6 +6,7 @@ const path = require('path');
 let dbStorageDir = __dirname  + path.sep + ['..', 'db_storage', ''].join(path.sep);
 
 function createBackup() {
+    createStorageDir();
     let currentDate = new Date();
     let dateVersion = currentDate.toISOString().replace(/-/g, '').replace(/:/g, '').replace('.','');
     fs.copyFileSync(__dirname + path.sep + '../seelsorge.db', dbStorageDir + 'seelsorgeBackup_'+dateVersion+'.db');
@@ -13,6 +14,22 @@ function createBackup() {
 
 function checkIsNewVersionOnline() {
     console.log("download new file");
+}
+
+function createStorageDir() {
+    const dirPath = "./db_storage";
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdir(dirPath, (err) => {
+            if (err) {
+                return console.error(err);
+            }
+            fs.chmod(dirPath, 0o777, (err) => {
+                if (err) throw err;
+                console.log('The permissions set for dir: db_storage');
+              });
+            console.log(dirPath + ' Directory created successfully!');
+        });
+    }
 }
 
 module.exports =  {
